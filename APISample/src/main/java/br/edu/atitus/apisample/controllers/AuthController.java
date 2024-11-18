b2766580c727212d7dc6c5ef408d25fd73105ce2
Pedro Henrique Piovezan <pedro.piovezan@outlook.com>
2024-11-18 19:22:34 -0300

    AuthController.java
    commit Pedro

--- APISample/src/main/java/br/edu/atitus/apisample/controllers/AuthController.java
+++ APISample/src/main/java/br/edu/atitus/apisample/controllers/AuthController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.edu.atitus.apisample.dtos.SignupDTO;
 import br.edu.atitus.apisample.entities.TypeUser;
 import br.edu.atitus.apisample.entities.UserEntity;
