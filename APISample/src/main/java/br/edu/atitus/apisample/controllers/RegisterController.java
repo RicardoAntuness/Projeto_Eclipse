b7495e7e2a22bcc3bb5112259bb5cf5bcbe8b685
RicardoAntuness <ricardo_rantunes22@hotmail.com>
2024-11-18 20:32:10 -0300

    commit.

--- APISample/src/main/java/br/edu/atitus/apisample/controllers/RegisterController.java
+++ APISample/src/main/java/br/edu/atitus/apisample/controllers/RegisterController.java
@@ -1,12 +1,16 @@
 package br.edu.atitus.apisample.controllers;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +36,7 @@ public class RegisterController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<RegisterEntity> createRegister(RegisterDTO registerDTO) throws Exception{
+	public ResponseEntity<RegisterEntity> createRegister(@RequestBody RegisterDTO registerDTO) throws Exception{
 		RegisterEntity newRegister= new RegisterEntity();
 		BeanUtils.copyProperties(registerDTO, newRegister);
 		
@@ -49,5 +53,28 @@ public class RegisterController {
 		var registers = service.findAll();
 		
 		return ResponseEntity.ok(registers);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<RegisterEntity> getOneRegister(@PathVariable UUID id) throws Exception{
+		var register = service.findById(id);
+		return ResponseEntity.ok(register);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<RegisterEntity> putRegister(@PathVariable UUID id, @RequestBody RegisterDTO dto) throws Exception{
+		RegisterEntity register = service.findById(id);
+		BeanUtils.copyProperties(dto, register);
+		
+		service.save(register);
+		
+		return ResponseEntity.ok(register);
+		}
+	
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handlerException (Exception ex){
+		String message = ex.getMessage().replaceAll("\r\n","" );
+		return ResponseEntity.badRequest().body(message);	
 	}
 }
